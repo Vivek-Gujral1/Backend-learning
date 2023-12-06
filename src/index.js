9cdd1bf8a6c7d20be89b1f274b2270242fa32d3c
Vivek-Gujral1 <vivekgujral2345@gmail.com>
2023-12-06 19:48:08 +0530

    working on backend and making utils folder , file

--- src/index.js
+++ src/index.js
@@ -3,11 +3,21 @@ import dotenv from "dotenv"
 
 
 import ConnectDB from "./dataBase/index.js";
+import { app } from "./app.js";
+const PORT = process.env.PORT || 6000
 
 dotenv.config({
     path :"./env"
 })
 
 ConnectDB()
+.then(()=>{
+    app.listen(PORT, ()=>{
+        console.log(`server is Running at port ${process.env.PORT} `);
+    })
+})
+.catch((err)=>{
+  console.log("MONGO CONNECION WITH APP FAILED", err);
+})
  
 
